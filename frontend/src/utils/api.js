626aa475b0d800843d073fdec9335645f2897d73
Renny Jose Espinosa <rennyespinosa@gmail.com>
2025-09-18 18:03:53 +0200

    cambio en api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,11 @@
-import axios from 'axios';
-const baseURL = (import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000').replace(/\/+$/, '');
-const api = axios.create({ baseURL });
+import axios from "axios";
+
+const isLocal = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
+
+const API_BASE = isLocal ? "http://localhost:8000" : "";
+
+const api = axios.create({
+  baseURL: `${API_BASE}/gis`,
+});
+
 export default api;
